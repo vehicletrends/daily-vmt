7263069f71d8b05aa2554c64d6198d8167e1d8eb
John Helveston <john.helveston@gmail.com>
2026-01-24 10:07:38 -0500

    clean up app

--- app.R
+++ app.R
@@ -4,7 +4,7 @@ library(echarts4r)
 library(tidyverse)
 
 # Load data
-data_url <- "https://cdn.jsdelivr.net/gh/vehicletrends/data@main/data/quantiles_dvmt.csv"
+data_url <- "https://raw.githubusercontent.com/vehicletrends/data/refs/heads/main/data/quantiles_dvmt.csv"
 data <- read.csv(data_url)
 
 ui <- page_sidebar(
@@ -29,61 +29,16 @@ ui <- page_sidebar(
       selected = unique(data$powertrain)
     )
   ),
-  card(
-    card_header("Legend"),
-    echarts4rOutput("legend_chart", height = "100px")
-  ),
   uiOutput("charts")
 )
 
 server <- function(input, output, session) {
-
   filtered_data <- reactive({
     data %>%
       filter(
         vehicle_type %in% input$vehicle_types,
         powertrain %in% input$powertrains
       )
-  })
-
-  # Create a minimal chart that shows only the legend
-  output$legend_chart <- renderEcharts4r({
-    req(nrow(filtered_data()) > 0)
-
-    # Determine which variable represents the lines
-    if (input$facet_by == "powertrain") {
-      line_var <- "vehicle_type"
-    } else {
-      line_var <- "powertrain"
-    }
-
-    # Create a chart with minimal data just to generate the legend
-    # We'll take just 2 points per series to make it tiny
-    legend_data <- filtered_data() %>%
-      group_by(.data[[line_var]]) %>%
-      slice(1:2)
-
-    legend_data %>%
-      group_by(.data[[line_var]]) %>%
-      e_charts(dvmt) %>%
-      e_line(quantile, symbol = "none", lineStyle = list(opacity = 0)) %>%
-      e_legend(
-        show = TRUE,
-        orient = "horizontal",
-        left = "center",
-        top = "center",
-        itemGap = 20
-      ) %>%
-      e_x_axis(show = FALSE) %>%
-      e_y_axis(show = FALSE) %>%
-      e_grid(
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
-        show = FALSE
-      ) %>%
-      e_tooltip(show = FALSE)
   })
 
   output$charts <- renderUI({
@@ -106,7 +61,7 @@ server <- function(input, output, session) {
     })
 
     layout_column_wrap(
-      width = 1/2,
+      width = 1 / 2,
       !!!chart_outputs
     )
   })
@@ -141,7 +96,13 @@ server <- function(input, output, session) {
             e_tooltip(trigger = "axis") %>%
             e_x_axis(name = "DVMT") %>%
             e_y_axis(name = "Quantile") %>%
-            e_legend(show = FALSE) %>%
+            e_legend(
+              show = TRUE,
+              orient = "horizontal",
+              left = "center",
+              bottom = 0
+            ) %>%
+            e_grid(bottom = 80) %>%
             e_color(background = "transparent") %>%
             e_toolbox_feature(feature = "saveAsImage")
         })
